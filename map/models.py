4cd3b835fcd35f110fa3680af6e76c051f474408
arturha4 <sadbooys.2001@gmail.com>
2022-02-03 17:42:04 +0500

    настроена модель комментариев,добавлен скелет отображения комменатриев

--- map/models.py
+++ map/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.utils import timezone
-
 from mappetproject import settings
 
-
+'''Модель точки'''
 class Point(models.Model):
     created_by=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     address=models.TextField()
@@ -16,9 +15,14 @@ class Point(models.Model):
     def __str__(self):
         return self.name
 
-
+'''Модель комментария'''
 class Comment(models.Model):
     text=models.TextField()
-    creator_name=models.CharField(max_length=30)
-    creator_id=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    author_id=models.CharField(max_length=30)
+    point_id=models.ForeignKey(Point, on_delete=models.CASCADE, default=None,related_name='comments')
     created_at=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
+    class Meta:
+        ordering=('created_at',)
